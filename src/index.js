82c86bfa69964d1c4a25bdae1b2c0d304462ddd1
Thiago <Thiagop@gmail.com>
2024-03-06 17:58:51 -0300

    dropped active users

--- src/index.js
+++ src/index.js
@@ -4,3 +4,20 @@ let names=['thiago','pereira','sousa']
 let newArrey=_.concat(names,'elma','clea','pereira')
 console.log(newArrey)
 
+let numbers1=[1,2,3,4,5,8]
+let numbers2=[2,6,7,1,3,4]
+let numbersDifference=_.difference(numbers1,numbers2)
+console.log(numbersDifference)
+
+const users=[
+  {user: 'thiago', active: true},
+  {user: 'betoven', active : true},
+  {user: 'pandora', active:false},
+  {user:'perola',active:false}
+]
+const activeusers=_.dropRightWhile(users,function(o){
+  return !o.active
+})
+console.log(activeusers)
+
+alert('something')
